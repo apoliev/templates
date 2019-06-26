b432b25a09189c6d61b30591bdb4bdebeece2ed0
Poliev Alexey <apoliev@rnds.pro>
2019-06-26 09:34:52 +0300

    update db script

--- shell/db.sh
+++ shell/db.sh
@@ -36,20 +36,41 @@ stop() {
     esac
 }
 
+list() {
+  list=($@)
+  echo "List of DBMS:"
+  for i in "${list[@]}"; do
+    echo "- $i"
+  done
+}
+
+default() {
+  echo "$1" > $2
+  cat $2
+}
+
 show_help() {
     help_text="Usage: db [COMMAND]...[[DBMS]]"
     help_text="$help_text\nCommand's list:\n"
     help_text="$help_text\tstart - start db server;\n"
     help_text="$help_text\tstop - stop db server;\n"
     help_text="$help_text\trestart - restart db server;\n"
+    help_text="$help_text\tlist - list of DBMS\n"
+    help_text="$help_text\tdefault - set default DBMS\n"
     help_text="$help_text\t--help - show help;\n"
     printf "$help_text"
 }
 
-dbms="maria"
+dbms_list=(maria postgres)
+config_file="$HOME/.db_runner"
+if [ -f "$config_file" ]; then
+  dbms="$(cat $config_file)"
+else
+  dbms="maria"
+fi
 
 if [ -n "$2" ]; then
-    dbms=$2
+  dbms=$2
 fi
 
 case "$1" in
@@ -64,6 +85,14 @@ case "$1" in
       restart)
         stop "$dbms"
         start "$dbms"
+        ;;
+
+      list)
+        list "${dbms_list[@]}"
+        ;;
+
+      default)
+        default $2 "$config_file"
         ;;
 
       --help)
