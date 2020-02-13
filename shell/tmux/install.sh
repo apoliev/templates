25a55161fad634721578749b19d6652a5f863f08
Poliev Alexey <apoliev@rnds.pro>
2020-02-13 18:11:18 +0300

    cleaning and tmux new config

--- shell/tmux/install.sh
+++ shell/tmux/install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp tmux.conf $HOME/.tmux.conf
--- shell/zsh/install.sh
+++ shell/zsh/install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cp -a zsh_conf $HOME/.zsh_conf
 cp zshrc $HOME/.zshrc
 cp bira-custom.zsh-theme $HOME/.oh-my-zsh/custom/themes/
