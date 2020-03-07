29e6b514a6ea1ff79d7a01ba3b8af6898d7b647f
Alexey Poliev <poliev.alex@gmail.com>
2020-03-07 17:54:40 +0300

    fix installer

--- shell/zsh/install.sh
+++ shell/zsh/install.sh
@@ -7,7 +7,7 @@ cp -a plugins/conda $HOME/.oh-my-zsh/custom/plugins
 cp -a plugins/rvm $HOME/.oh-my-zsh/custom/plugins
 
 echo "copy themes"
-cp -R themes $HOME/.oh-my-zsh/custom/themes
+cp themes/bira-custom.zsh-theme $HOME/.oh-my-zsh/custom/themes
 
 echo "copy base zsh scripts"
 cp ruby_prompt.zsh $HOME/.oh-my-zsh/custom
