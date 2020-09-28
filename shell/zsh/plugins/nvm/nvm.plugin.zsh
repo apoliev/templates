647a9d5b8a2ddf743988718577848fe3a09a8690
Poliev Alexey <poliev.alex@gmail.com>
2020-09-28 20:58:54 +0300

    env vars

--- shell/zsh/plugins/nvm/nvm.plugin.zsh
+++ shell/zsh/plugins/nvm/nvm.plugin.zsh
@@ -3,7 +3,7 @@ nvm_dir="$HOME/.nvm"
 if [ -d "$nvm_dir" ]; then
   function nvm_prompt() {
     current="$(nvm version current)"
-    default="$(nvm version default)"
+    default="$NVM_DEFAULT_ENV"
     if [ "$default" != "$current" ]; then
       echo "${ZSH_THEME_NVM_PROMPT_PREFIX}nvm-${current}${ZSH_THEME_NVM_PROMPT_SUFFIX}"
     fi
@@ -11,4 +11,6 @@ if [ -d "$nvm_dir" ]; then
 
   export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  
+  export NVM_DEFAULT_ENV="$(nvm version default)"
 fi 
--- shell/zsh/plugins/rvm/rvm.plugin.zsh
+++ shell/zsh/plugins/rvm/rvm.plugin.zsh
@@ -1,9 +1,10 @@
 rvm_dir="$HOME/.rvm"
 
 if [ -d "$rvm_dir" ]; then
+
   function rvm_prompt() {
-    current="$(rvm current)"
-    default="$(rvm alias show default)"
+    current="$RUBY_VERSION"
+    default="$RVM_DEFAULT_ENV"
     if [ "$default" != "$current" ]; then
       echo "${ZSH_THEME_RUBY_PROMPT_PREFIX}${current}${ZSH_THEME_RUBY_PROMPT_SUFFIX}"
     fi
@@ -13,6 +14,8 @@ if [ -d "$rvm_dir" ]; then
   [[ -s "$rvm_dir/scripts/rvm" ]] && source "$rvm_dir/scripts/rvm" 
 
   [[ -d "$rvm_dir" ]] && export PATH="$PATH:$rvm_dir/bin"
+  
+  export RVM_DEFAULT_ENV="$(rvm alias show default)"
   # <<< rvm initialize <<<
 else
   echo "RVM directory not found"
